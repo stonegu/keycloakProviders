fc59d42127092a111d402a2562732f7afc4170a3
Stone Gu <stone_gu@hotmail.com>
2019-02-20 23:18:15 -0500

    customized rest service

--- src/main/java/com/bizislife/keycloak/rest/UserResource.java
+++ src/main/java/com/bizislife/keycloak/rest/UserResource.java
@@ -3,10 +3,15 @@ package com.bizislife.keycloak.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.annotations.cache.NoCache;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.representations.idm.UserRepresentation;
 
